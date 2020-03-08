8e41a1c4b0dd5ede247533b8c094a1693a1a6d76
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-07 22:24:11 -0300

    feat: adição regra email

--- validation.js
+++ validation.js
@@ -9,6 +9,7 @@ const patterns = {
   username: /^[a-z\d]{5,12}$/,
   password: /^[\w@-]{8,20}$/,
   slug: /^[a-z\d-]{8,20}$/,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
 }
 
 // validation funciton
